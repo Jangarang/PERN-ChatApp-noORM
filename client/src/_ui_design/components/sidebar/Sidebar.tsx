829b6518fa75766a8558c3ca822994b4f27a9443
John <gamuedajohn@outlook.com>
2025-06-04 13:21:04 -0600

    sidebar initial design complete

--- client/src/_ui_design/components/sidebar/Sidebar.tsx
+++ client/src/_ui_design/components/sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import SearchInput from "./SearchInput";
 import LogoutButton from "./Logout";
+import Conversations from "./Conversations";
 const Sidebar = () => {
     return (
         <aside className='border-r border-slate-500 p-1 md:p-4 flex flex-col w-44 md:w-1/2'>
             <SearchInput/>
             <p>Hello</p>
+            <Conversations/>
             <LogoutButton/>
 
         </aside>
